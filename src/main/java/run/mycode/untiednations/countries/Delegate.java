ca74041d739bee5aaaae2dbcbd259a7454130c2b
mrbdahlem <DAHLEM.BRIAN@cusd80.com>
2018-11-20 08:46:41 -0700

    updated description of a delegate

--- src/main/java/run/mycode/untiednations/countries/Delegate.java
+++ src/main/java/run/mycode/untiednations/countries/Delegate.java
@@ -2,15 +2,16 @@
  * The diplomatic interface for a country.
  * 
  * A country in the Untied Nations General Assembly needs to be able perform
- * three operations. The country must be able to declare its name, declare war,
- * and receive other countries declarations of war.
+ * four operations. The country must be able to declare its name, record the 
+ * wealth of all nations, declare war, and receive other countries declarations
+ * of war.
  * 
- * Each meeting of the General Assembly starts with a roll call. The delegates
- * will each declare the name of the country they represent. After the roll 
- * call, the current wealth of each country will be tallied and reported to the
- * delegates. Next, a silent, binding poll will be taken in which each country
- * will record their wish to go to war with each other country. Once the poll is
- * taken, wars will be announced. The meeting will then end.
+ * The first meeting of the General Assembly starts with a roll call. The 
+ * delegates will each declare the name of the country they represent. After the
+ * roll call, the current wealth of each country will be tallied and reported to
+ * the delegates. Next, a silent, binding poll will be taken in which each 
+ * country will record their wish to go to war with each other country. Once the
+ * poll is taken, wars will be announced. The meeting will then end.
  * 
  * After all wars are fought, the resource distribution will be tallied and
  * another meeting will be called.
